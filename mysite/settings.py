6e25568992ead428884cc5983a38f219ddd681a4
LMhao677 <2765227676@qq.com>
2025-09-08 16:36:31 +0800

    part8

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-shanghai'
+TIME_ZONE = 'Asia/Shanghai'
 
-TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
